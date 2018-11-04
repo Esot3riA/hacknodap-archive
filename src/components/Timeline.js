2cd43c327f4489fe785a1f50a025e838184b8087
Esot3riA <esot3ria@gmail.com>
2018-11-04 23:30:22 +0000

    Add Awesome grid style background

---
+++ src/components/Timeline.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Timeline.scss';
+
+class Timeline extends Component {
+  
+  render() {
+    return (
+      <div className="timeline">
+        <div className="timeline-grid">
+          
+        </div>
+      </div>
+    );
+  }
+  
+}
+
+export default Timeline;
