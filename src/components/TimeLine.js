accd1a1e86adbe546f36f63a655ca287b1c4ecab
Esot3riA <esot3ria@gmail.com>
2018-11-07 09:12:45 +0000

    Rebuild structure and Add blur effect

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -4,14 +4,10 @@ import './TimeLine.scss';
 class TimeLine extends Component {
   render() {
     return(
-      <div className="timeline">
-        <div className="backbone-wrap">
-          <div className="backbone">
-            <div className="historydot"></div>
-            <div className="historydot"></div>
-            <div className="historydot"></div>
-          </div>
-          <div className="backbone-end">
+      <div className="timeline-wrap">
+        <div className="timeline">
+          <div className="history-container top">
+            <div className="history">blahblah</div>
           </div>
         </div>
       </div>
