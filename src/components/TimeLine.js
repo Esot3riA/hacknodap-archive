9f644ae9ecaf566d7569afde506adc509011b08a
Esot3riA <esot3ria@gmail.com>
2018-11-08 23:31:07 +0000

    Renew history design

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -7,7 +7,11 @@ class TimeLine extends Component {
       <div className="timeline-wrap">
         <div className="timeline">
           <div className="history-container top">
-            <div className="history">blahblah</div>
+            <div className="head">
+                <div className="date">2018.11.01</div>
+                <div className="title">핵노답 아카이브 개발 시작</div>
+            </div>
+            <div className="body">:)</div>
           </div>
           <div className="history-container bottom">
             <div className="history">another blah</div>
