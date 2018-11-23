d6305e3427ba806d08f310845b56a486c7786015
Esot3riA <esot3ria@gmail.com>
2018-11-23 08:15:58 +0000

    import material ui design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import './App.scss';
-import Header from './components/Header';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import GridTable from './components/GridTable';
+import './App.scss';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: deepOrange[700],
+    },
+  }
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <MuiThemeProvider theme={theme}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit">
+                Hacknodap Archive
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </MuiThemeProvider>
         <GridTable />
       </div>
     );
