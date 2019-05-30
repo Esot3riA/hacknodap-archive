2aa8b3140a9b5fb925a2b1cc55858eb69855fca0
Esot3riA <esot3ria@gmail.com>
2019-05-30 16:15:28 +0900

    Remove env files

---
+++ hacknodap-archive/src/components/HistoryDialog/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import { Properties } from '../../config/properties';
+import styled from 'styled-components';
+
+const Picture = styled.div`
+  width: 400px;
+  height: 300px;
+  background-image: url(${props => props.restAPIURL + props.imageURL});
+	background-size: contain;
+  background-repeat: no-repeat;
+  background-position-x: center;
+  margin: 20px 0;
+`;
+
+const HistoryDialog = ({ open, onClose, history }) => {
+  const title = history.get('title');
+  const imageURLs = history.get('imageURL').toJS();
+  const PictureList = imageURLs.map(
+    (imageURL) => (
+      <Picture
+        restAPIURL={Properties.restAPIURL}
+        imageURL={imageURL} />)
+  );
+  
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      scroll="paper">
+      <DialogTitle>
+        {title}
+      </DialogTitle>
+      <DialogContent dividers="true">
+        {PictureList}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Ok
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default HistoryDialog;
