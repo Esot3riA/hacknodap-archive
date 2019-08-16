cb171bd6dbf3e990d55630d0182f2f9ddbaad436
Esot3riA <esot3ria@gmail.com>
2019-08-17 03:05:52 +0900

    Fix safari CORS bug

--- hacknodap-archive-server/app.js
+++ hacknodap-archive-server/app.js
@@ -20,7 +20,7 @@ app.use(express.static('static'));
 // Handle CORS.
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, UPDATE, DELETE");
   res.header("Access-Control-Allow-Headers", 
              "Origin, X-Requested-With, Content-Type, Accept");
   next();
